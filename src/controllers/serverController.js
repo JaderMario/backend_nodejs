075798dbd4d38b90f1e642535fabe175292fbd16
jadermario3377 <jade.m77@gmail.com>
2021-10-08 15:04:26 -0500

    Se agrega código en los archivos del src

--- src/controllers/serverController.js
+++ src/controllers/serverController.js
@@ -1,3 +1,10 @@
+let users = [
+    {id: 1, nombre: "Geremias", apellido: "Beltran"},
+    {id: 2, nombre: "Isadora", apellido: "Montiel"},
+    {id: 3, nombre: "Laureano", apellido: "Gómez"},
+    {id: 4, nombre: "Carlos", apellido: "Castro"}
+] 
+
 class ServerController{
     constructor(){
 
@@ -5,7 +12,7 @@ class ServerController{
 
     register(req, res){
         //obtener datos
-        let { nombre, apellido } = req.body;
+        let { id, nombre, apellido } = req.body;
         console.log("Usuario registrado con éxito");
         console.table({nombre, apellido});
         res.status(200).json({
@@ -13,14 +20,47 @@ class ServerController{
         });
     }
     
+    update(req, res){
+        let { id, nombre, apellido } = req.body;
+        console.table({id, nombre, apellido});
+        
+        users.forEach(element => {
+            if(id == element.id){
+                element.nombre = nombre;
+                element.apellido = apellido;
+            }
+        });
+
+        if(users != null){
+            res.status(200).json(users);
+        }else{
+            res.status(400).json({message: "Usuario no encontrado"});
+        }
+
+    }
+
+    deleteUser(req, res){
+        let { id } = req.body;
+        console.table({id});
+
+        let tempUser = [];
+        users.forEach(element => {
+            if(id != element.id){
+                tempUser.push(element);
+            }
+        });
+
+        users = tempUser;
+        if(users != null){
+            res.status(200).json(users);
+        }else{
+            res.status(400).json({message: "Usuario no encontrado"});
+        }
+    }
+
     getUsers(req, res){
         let id = req.params.id;
-        let users = [
-            {id: 1, nombre: "Geremias", apellido: "Beltran"},
-            {id: 2, nombre: "Isadora", apellido: "Montiel"},
-            {id: 3, nombre: "Laureano", apellido: "Gómez"},
-            {id: 4, nombre: "Carlos", apellido: "Castro"}
-        ]
+        
         let userResp = null;
         users.forEach(element => {
             if(id == element.id){
@@ -33,17 +73,9 @@ class ServerController{
         }else{
             res.status(400).json({message: "Usuario no encontrado"});
         }
-
     }
 
-    getAllUsers(req, res) {
-        let users = [
-            {id: 1, nombre: "Geremías", apellido: "Beltrán"},
-            {id: 2, nombre: "Isadora", apellido: "Montiel"},
-            {id: 3, nombre: "Laureano", apellido: "Gómez"},
-            {id: 4, nombre: "Carlos", apellido: "Castro"}
-        ]
-
+    getAllUsers(req, res) {        
         res.status(200).json(users);
     }
 }
